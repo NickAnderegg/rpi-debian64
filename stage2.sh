00279712f0cc68d3640fca8264d979f0c2bff272
Nick Anderegg <nick@anderegg.io>
2020-04-04 22:25:18 +0000

    Add default console-setup settings

--- stage2.sh
+++ stage2.sh
@@ -1,7 +1,20 @@
 # Apt install core packages
 
+cat << EOF > /etc/defaults/console-setup
+# Consult the console-setup(5) manual page.
+ACTIVE_CONSOLES="/dev/tty[1-6]"
+
+CHARMAP="UTF-8"
+
+CODESET="Lat15"
+FONTFACE=""
+FONTSIZE=""
+VIDEOMODE=
+EOF
+
+
 apt update
-apt upgrade -y 
+apt upgrade -y
 apt install console-setup keyboard-configuration sudo ssh curl wget dbus usbutils ca-certificates crda less fbset debconf-utils avahi-daemon fake-hwclock nfs-common apt-utils man-db pciutils ntfs-3g apt-listchanges -y
 apt install wpasupplicant wireless-tools firmware-atheros firmware-brcm80211 firmware-libertas firmware-misc-nonfree firmware-realtek dhcpcd5 net-tools -y
 
